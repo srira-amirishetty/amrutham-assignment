055ad3583c4a5e386570128d30373b4ef19e937e
srira-amirishetty <sriramamirishetty@gmail.com>
2025-08-17 12:39:56 +0530

    cors issue

--- backend/index.js
+++ backend/index.js
@@ -15,12 +15,9 @@ const errorHandler = require('./middlewares/errorHandler');
 const app = express();
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://amrutham-assignment.onrender.com"
-  ],
-  credentials: true
-}));
+    origin: '*',
+    credentials: true
+}))
 
 app.use(express.json());
 
